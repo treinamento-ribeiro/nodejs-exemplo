4616e1ed5e109d39d27d822d5ea448bbde5148b2
asilvaribeiro <asilvaribeiro@prodam.sp.gov.br>
2025-08-26 11:23:42 -0300

    Workflow deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Teste</h1>
+      <div>
+        <h1>Treinamento Github</h1>
+        <h2>Ribeiro</h2>
+      </div>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
